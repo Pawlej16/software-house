88f5397dc78dec8f9f6d726c25c2d263cb7cf8ba
Pawlej16 <pawel.sztenke@gmail.com>
2026-04-21 12:35:49 +0200

    Created classes and finished the code in the Employee class

---
+++ src/Employee.java
@@ -0,0 +1,51 @@
+public class Employee {
+
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private double baseSalary;
+
+    public Employee(String employeeId, String firstName, String lastName, double baseSalary) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.baseSalary = baseSalary;
+    }
+
+    public final String getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirstName()  { return firstName; }
+    public String getLastName()   { return lastName; }
+    public double getBaseSalary() { return baseSalary; }
+
+    public double calculateMonthlyCost() {
+        return baseSalary;
+    }
+
+    public String introduceYourself() {
+        return "Hi, I am " + firstName + " " + lastName + " (ID: " + employeeId + ").";
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName()
+                + " [id=" + employeeId
+                + ", name=" + firstName + " " + lastName
+                + ", baseSalary=" + baseSalary + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Employee)) return false;
+        Employee other = (Employee) obj;
+        return employeeId.equals(other.employeeId);
+    }
+
+    @Override
+    public int hashCode() {
+        return employeeId.hashCode();
+    }
+}
